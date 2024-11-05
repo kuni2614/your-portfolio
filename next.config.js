b431c91f179fbee1fe72ff41c76cadc4a4daeb05
Kunika <kunika.professional@gmail.com>
2024-11-05 02:07:23 +0000

    Configure static export and deployment to GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+module.exports = {
+    output: 'export',
+    images: {
+      unoptimized: true // Optional: Use this if you encounter issues with image optimization on static export
+    }
+  };  
